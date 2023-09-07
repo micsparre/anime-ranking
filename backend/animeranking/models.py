b9e68a86a698e9165ae5c0b7150eadb28ad29f76
Michael Sparre <67245304+micsparre@users.noreply.github.com>
2023-09-07 16:17:24 -0700

    updated backend to handle post request for title creations

--- backend/animeranking/models.py
+++ backend/animeranking/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Title(models.Model):
-    media_id = models.IntegerField()  # Replace 'IntegerField' with the actual data type for 'media_id'
+    media_id = models.IntegerField(unique=True)  # Replace 'IntegerField' with the actual data type for 'media_id'
     title = models.CharField(max_length=255)  # Replace 'CharField' and set the max length as needed
 
     def __str__(self):
--- backend/animeranking/urls.py
+++ backend/animeranking/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('titles/', views.get_titles_from_anilist, name='get_titles_from_anilist'),
-    path('create_title/', views.create_title, name='create_title'),
+    path('titles', views.get_titles_from_anilist, name='get_titles_from_anilist'),
+    path('create_title', views.create_title, name='create_title'),
     # path('anime/', views.AnimeListCreateView.as_view(), name='anime-list-create'),
     # path('user-ranking/', views.UserAnimeRankingListCreateView.as_view(), name='user-ranking-list-create'),
 ]
