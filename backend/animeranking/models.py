ff80b7edacf4dcc05047018559f50b9d8bffe969
Michael Sparre <michaelsparre@berkeley.edu>
2023-09-07 12:00:58 -0700

    removed model / serializing

--- backend/animeranking/models.py
+++ backend/animeranking/models.py
@@ -1,22 +1,2 @@
-from django.db import models
 
-class Anime(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    release_date = models.DateField()
-    # Add more fields as needed
-
-class UserAnimeRanking(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
-    ranking = models.PositiveIntegerField()
-    # Add more fields as needed
-    
-class Title(models.Model):
-    name = models.CharField(max_length=255)
-    media_id = models.IntegerField(default=0)
-    def __str__(self):
-        return self.name
-    
-        
     
--- backend/animeranking/views.py
+++ backend/animeranking/views.py
@@ -1,17 +1,5 @@
 # appname/views.py
 
-from rest_framework import generics
-from .models import Anime, UserAnimeRanking, Title
-from .serializers import AnimeSerializer, UserAnimeRankingSerializer, TitleSerializer
-
-class AnimeListCreateView(generics.ListCreateAPIView):
-    queryset = Anime.objects.all()
-    serializer_class = AnimeSerializer
-
-class UserAnimeRankingListCreateView(generics.ListCreateAPIView):
-    queryset = UserAnimeRanking.objects.all()
-    serializer_class = UserAnimeRankingSerializer
-
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -19,28 +7,13 @@ from .api_integration import fetch_data_from_api
 
 @api_view(['GET'])
 def get_titles_from_anilist(request):
-    print(f"request: {request}")
-    
     # Get the 'title' query parameter from the request
     title_query = request.query_params.get('title', None)
-    
-    print(f"title_query: {title_query}")
-
+    filtered_data = []
     if title_query is not None:
         # Call the function to fetch data from the third-party API
         external_data = fetch_data_from_api(title_query)
-
-        # Save the external data to your Title model
         for item in external_data:
             if item.get('title').get('english') is not None:
-                Title.objects.create(name=item.get('title').get('english'), media_id=item.get("id"))
-
-        # Retrieve and serialize the data from your Title model
-        queryset = Title.objects.all()
-        serializer = TitleSerializer(queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    else:
-        # If no 'title' parameter is provided, return all data from your Title model
-        queryset = Title.objects.all()
-        serializer = TitleSerializer(queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+                filtered_data.append({'id': item.get('id'), 'title': item.get('title').get('english')})
+    return Response(filtered_data, status=status.HTTP_200_OK)
