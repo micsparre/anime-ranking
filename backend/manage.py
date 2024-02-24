a31ecc795eced3dd54846a281e60f10cce21f380
Michael Sparre <micsparre@gmail.com>
2024-02-23 22:04:06 -0800

    fixed env variables setup for local development

--- backend/manage.py
+++ backend/manage.py
@@ -2,13 +2,15 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv, find_dotenv
 
 
 def main():
     """Run administrative tasks."""
-    if os.environ.get("DEBUG", False):
+    if os.environ.get('DEBUG', False):
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.prd_settings')
     else:
+        load_dotenv(find_dotenv(".env.development"))
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.dev_settings')
     try:
         from django.core.management import execute_from_command_line
