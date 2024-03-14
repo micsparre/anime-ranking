388d92f15b6fb2fe5862ee85fa944fa207163a81
Michael Sparre <micsparre@gmail.com>
2024-03-14 16:33:28 -0700

    update openai model to latest release; gpt-4-turbo-preview

--- backend/animeranking/openai_api.py
+++ backend/animeranking/openai_api.py
@@ -30,7 +30,7 @@ def get_recommendations_as_list(ranked_titles: list, bookmark_titles: list) -> l
         ranked_titles), bookmark_titles=", ".join(bookmark_titles))
     print(f"openai query: {content}")
     completion = client.chat.completions.create(
-        model="gpt-4-1106-preview", messages=[{"role": "user", "content": content}])
+        model="gpt-4-turbo-preview", messages=[{"role": "user", "content": content}])
     try:
         response = completion.choices[0].message.content
         response = json.loads(response)
